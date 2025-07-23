c6b24d4f774b19244150ca5d80ddaf22be270242
Marcos Yacob <marcos@spirl.com>
2025-07-23 12:14:26 -0300

    foo commit 2

--- foo-test/main.go
+++ foo-test/main.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("foo-test projects")
+	fmt.Printf("foo-test projects: %v\n", time.Now().Format("2006-01-02 15:04:05"))
 }
