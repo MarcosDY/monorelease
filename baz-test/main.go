112c7c6c7ba92268bc2d86ad9e6bc4573524081e
Marcos Yacob <marcos@spirl.com>
2025-07-23 12:15:37 -0300

    baz commit 3

--- baz-test/main.go
+++ baz-test/main.go
@@ -5,6 +5,10 @@ import (
 	"time"
 )
 
-func main() {
+func main(args []string) {
+	if len(args) < 1 {
+		fmt.Println("Usage: baztest <project>")
+		return
+	}
 	fmt.Printf("baz-test projects: %v\n", time.Now().Format("2006-01-02 15:04:05"))
 }
