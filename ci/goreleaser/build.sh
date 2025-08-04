f23404ff71911df9570a9c2cb6491a6652841232
Marcos Yacob <marcos@spirl.com>
2025-08-04 13:07:19 -0300

    test bazel

--- ci/goreleaser/build.sh
+++ ci/goreleaser/build.sh
@@ -3,6 +3,13 @@
 set -euo pipefail
 
 echo "This is a test script to demonstrate error handling."
-# Force an error
 
+echo "MISE_TRUSTED_CONFIG_PATHS: $MISE_TRUSTED_CONFIG_PATHS"`
+echo "MISE_YES: $MISE_YES"
+echo "MISE_LOG_LEVEL: $MISE_LOG_LEVEL"
 bazel info
+
+bazel build //:test_target || {
+  echo "Bazel build failed. Exiting script."
+  exit 1
+}
